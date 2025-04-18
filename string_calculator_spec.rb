129af22f3eb51da5688e02a0cca54875ae387469
vikas pandey <vikaspandey1099@gmail.com>
2025-04-18 12:21:02 +0530

    Added a failing test case for empty string

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -3,5 +3,9 @@ require_relative 'string_calculator'
  
 RSpec.describe StringCalculator do
   describe '#add' do
+    it "hendle empty string and retun 0" do
+      string_calculator = StringCalculator.new("")
+      expect(string_calculator.add).to eq(0)
+    end
   end
 end
